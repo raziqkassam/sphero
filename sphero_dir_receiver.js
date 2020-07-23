39d60a747114747224a2c9f5c5019a0895b6ed25
raziqkassam <raziq.kassam@gmail.com>
2020-07-23 15:59:43 -0600

    updated with sentence search ability (for multiple prints)

--- sphero_dir_receiver.js
+++ sphero_dir_receiver.js
@@ -19,9 +19,17 @@ const sensor = spawn('python', ['sphero_dir_thrower.py']);
 
 sensor.stdout.on('data', function(data) {
     
-    // Coerce Buffer object to Float
-    directions.push(parseFloat(data));
-    
+    var str = String(data);
+    var search_for = 'Predicted Value:'; // specific line with predicted value
+	var word_end = search_for.length + 1; // character number to find the value
+	
+    for( var i = 0; i < str.length; i++ ) {
+		if( str.substring(i,i+16) == search_for ) { // find line that is what you are searching for
+			console.log( str.substring(i, i + word_end + 1 ) ); // print out line with value
+			directions.push( parseFloat( str[ i + word_end ] )); // add the passed number to the array
+		}
+	}
+	
 	sprk.setBackLed( 255 ); 
 	
 	if( directions[ directions.length - 1 ] == 1 ) {
@@ -42,13 +50,17 @@ sensor.stdout.on('data', function(data) {
 		sprk.roll(0,heading); // no speed, therefore will only rotate
 
 	}
-	else {
+	else if( directions[ directions.length - 1 ] == 3 ) {
 		console.log( 'Forward' )
 			
-		sprk.setMotionTimeout( 1300 ); // only move sphero for specific time in ms
+		sprk.setMotionTimeout( 400 ); // only move sphero for specific time in ms
 		sprk.roll( speed, heading ); // speed can be changed in line 30
 			
 	}
+	else {
+		console.log( "Error - value was not found" );
+	}
+	
     // Log to debug
     console.log(directions);
 
--- sphero_dir_thrower.py
+++ sphero_dir_thrower.py
@@ -2,37 +2,37 @@
 import time
 import random
 
-cw_probability  = 40 # %
+cw_probability  = 40 
 ccw_probability = 40 
 fwd_probability = 20
 
-upper_limit = cw_probability + ccw_probability + fwd_probability
+upper_limit = cw_probability + ccw_probability + fwd_probability # totals
 
-limit_1 = 0 + cw_probability
+limit_1 = 0 + cw_probability 
 limit_2 = 100 - fwd_probability
 
-direction = 0
+direction = 0 # initialize variable to keep track
 
 while True :
-    number = random.randint(1,upper_limit)
-    time.sleep(3)
+    number = random.randint(1,upper_limit) # generate random number between 1 and upper_limit
+    time.sleep(3) # wait 3 seconds before outputting random number
     
     if number < limit_1 :
-        #print( "Analyzed 13Hz \nSphero will now rotate clockwise" )
-        #keyboard.press_and_release('d')
-        direction = 1
-        print( direction, flush=True, end='')
+        print( "Analyzed 13Hz \nSphero will now rotate clockwise" ) # printing distraction
+        direction = 1 # update variable to print
+        print( "Predicted Value:", direction, flush=True, end='') # print code with settings
         
     if limit_1 <= number < limit_2 :
-        #print( "Analyzed 19Hz \nSphero will now rotate counter clockwise" )
-        #keyboard.press_and_release('a')
+        print( "Analyzed 19Hz \nSphero will now rotate counter clockwise" )
         direction = 2
-        print( direction, flush=True, end='')
+        print( "Predicted Value:", direction, flush=True, end='')
         
     if number >= limit_2 :
-        #print( "Analyzed 25Hz \nSphero will now move forward" )
-        #keyboard.press_and_release('w')
+        print( "Analyzed 25Hz \nSphero will now move forward" )
         direction = 3
-        print( direction, flush=True, end='')
+        print( "Predicted Value:", direction, flush=True, end='')
         
-
+# LEGEND
+# 1 --> CLOCKWISE
+# 2 --> COUNTER CLOCKWISE
+# 3 --> FORWARD
