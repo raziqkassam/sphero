33cf98623e965d10bc10d8cba82c2a58894d9364
raziqkassam <raziq.kassam@gmail.com>
2020-07-29 14:18:49 -0600

    modified stream data to send values to .txt file

--- sphero_dir_receiver.js
+++ sphero_dir_receiver.js
@@ -1,3 +1,6 @@
+// Author: Raziq Kassam
+// sudo node sphero_dir_receiver.js
+
 var mac_id = "C5:9C:75:02:2A:E2"; // BLE ID for the specific Sphero SPRK+ (SK-2AE2)
 var sphero = require( "sphero" ),
 	sprk = sphero( mac_id ); 
@@ -10,61 +13,59 @@ var speed = 200; // speed of sphero when driving forward
 var heading = 0; // initialize and default set heading to 0 (forward)
 var rotation_angle = 45; // set the value of angle change on each side arrow click
 
-sprk.setRgbLed({ red: 0, green: 0, blue: 0 });
+sprk.setRgbLed({ red: 0, green: 0, blue: 0 }); // turn off the main LED's
+sprk.setBackLed( 255 ); // turn on the blue rear LED
 
 const { spawn } = require('child_process');
-const directions = []; // Store readings
+const fs = require('fs');
 
-const sensor = spawn('python', ['sphero_dir_thrower.py']);
+//const sensor = spawn('python3', ['sphero_dir_thrower.py']);
+const sensor = spawn('python3', [ 'stream_EEG_data.py' ]);
+
+var directions = []; // Store readings
+var index = 0;
+var stream_start = false;
 
 sensor.stdout.on('data', function(data) {
-    
-    var str = String(data);
-    var search_for = 'Predicted Value:'; // specific line with predicted value
-	var word_end = search_for.length + 1; // character number to find the value
-	
-    for( var i = 0; i < str.length; i++ ) {
-		if( str.substring(i,i+16) == search_for ) { // find line that is what you are searching for
-			console.log( str.substring(i, i + word_end + 1 ) ); // print out line with value
-			directions.push( parseFloat( str[ i + word_end ] )); // add the passed number to the array
+	// console.log( String(data) );
+	if( !stream_start && String(data) == 'Simulate Online is True' ) {
+		console.log( 'Stream is starting... \nBeginning to find predicted values\n\n' );
+		stream_start = true;
+	}
+   
+	if( stream_start ) {
+		
+		var directions = fs.readFileSync('directions.txt').toString().split("\n");
+
+		if( directions[ directions.length - 1 ] == '1' ) {
+			console.log( 'Found 1 --> Rotate Clockwise' )
+		
+			heading = heading + rotation_angle; // change the angle clockwise
+			heading = (360 + heading)%360;
+		
+			sprk.roll(0,heading);
+		
 		}
-	}
-	
-	sprk.setBackLed( 255 ); 
-	
-	if( directions[ directions.length - 1 ] == 1 ) {
-		console.log( 'Clockwise' )
-		
-		heading = heading + rotation_angle; // change the angle clockwise
-		heading = (360 + heading)%360;
-		
-		sprk.roll(0,heading);
+		else if( directions[ directions.length - 1 ] == '2' ) {
+			console.log( 'Found 2 --> Rotate Counter Clockwise' )
 			
-	}
-	else if( directions[ directions.length - 1 ] == 2 ) {
-		console.log( 'Counter Clockwise' )
-		
-		heading = heading - rotation_angle; // change the angle counter-clockwise
-		heading = (360 + heading)%360; // make sure the heading is in the domain of 0<heading<359
-			
-		sprk.roll(0,heading); // no speed, therefore will only rotate
+			heading = heading - rotation_angle; // change the angle counter-clockwise
+			heading = (360 + heading)%360; // make sure the heading is in the domain of 0<heading<359
+				
+			sprk.roll(0,heading); // no speed, therefore will only rotate
 
-	}
-	else if( directions[ directions.length - 1 ] == 3 ) {
-		console.log( 'Forward' )
+		}
+		else if( directions[ directions.length - 1 ] == '3' ) {
+			console.log( 'Found 3 --> Driving Forward' )
 			
-		sprk.setMotionTimeout( 400 ); // only move sphero for specific time in ms
-		sprk.roll( speed, heading ); // speed can be changed in line 30
-			
+			sprk.setMotionTimeout( 400 ); // only move sphero for specific time in ms
+			sprk.roll( speed, heading ); // speed can be changed in line 30
+					
+		}
+		else if ( directions[ directions.length - 1 ] == '4' ) {
+			console.log( 'Found 4 --> Resting' );
+		}
+		
+		console.log( directions ); // log to debug
 	}
-	else {
-		console.log( "Error - value was not found" );
-	}
-	
-    // Log to debug
-    console.log(directions);
-
 })
-
-
-
--- sphero_dir_thrower.py
+++ sphero_dir_thrower.py
@@ -13,24 +13,41 @@ limit_2 = 100 - fwd_probability
 
 direction = 0 # initialize variable to keep track
 
+new_file = open("directions.txt", "w+")
+new_file.close()
+
+count = 0
+print( "Simulate Online is True", flush=True, end='' )
 while True :
     number = random.randint(1,upper_limit) # generate random number between 1 and upper_limit
     time.sleep(3) # wait 3 seconds before outputting random number
     
+    new_file = open("directions.txt", "a+")
+    
+    
     if number < limit_1 :
-        print( "Analyzed 13Hz \nSphero will now rotate clockwise" ) # printing distraction
+        print( "\nAnalyzed 13Hz \nSphero will now rotate clockwise" ) # printing distraction
         direction = 1 # update variable to print
         print( "Predicted Value:", direction, flush=True, end='') # print code with settings
         
     if limit_1 <= number < limit_2 :
-        print( "Analyzed 19Hz \nSphero will now rotate counter clockwise" )
+        print( "\nAnalyzed 19Hz \nSphero will now rotate counter clockwise" )
         direction = 2
         print( "Predicted Value:", direction, flush=True, end='')
         
     if number >= limit_2 :
-        print( "Analyzed 25Hz \nSphero will now move forward" )
+        print( "\nAnalyzed 25Hz \nSphero will now move forward" )
         direction = 3
         print( "Predicted Value:", direction, flush=True, end='')
+
+    if count == 0 :
+            new_file.write( "%d" % direction ) 
+    else :
+        new_file.write( "\n%d" % direction )    
+    
+    count = count + 1
+    new_file.close()    
+     
         
 # LEGEND
 # 1 --> CLOCKWISE
